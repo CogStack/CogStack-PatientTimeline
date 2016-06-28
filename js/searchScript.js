bca02a1bf1b62323a0155e903d59570a454653ce
AliAliyev <aaliyev13@yahoo.com>
2016-06-28 12:38:01 +0100

    bottom and top margins added to pdf

--- js/searchScript.js
+++ js/searchScript.js
@@ -203,7 +203,7 @@ function createPDF(sourceText) {
 }
 
 function pdfToHTML(source){
-var pdf = new jsPDF('p', 'pt', 'letter');
+var pdf = new jsPDF('p', 'pt', 'a4');
 //source = $('#pdf2htmldiv')[0];
 specialElementHandlers = {
 	'#bypassme': function(element, renderer){
@@ -213,22 +213,25 @@ specialElementHandlers = {
 margins = {
     top: 50,
     left: 60,
-	right: 60,
-    width: 500
+    width: 480
   };
 pdf.fromHTML(
   	source // HTML string or DOM elem ref.
   	, margins.left // x coord
   	, margins.top // y coord
   	, {
-  		'width': margins.width // max width of content on PDF
+  		'width': margins.width // max width of content on PDF 
   		, 'elementHandlers': specialElementHandlers
   	},
   	function (dispose) {
   	  // dispose: object with X, Y of the last line add to the PDF
   	  //          this allow the insertion of new lines after html
         pdf.save('html2pdf.pdf');
-      }
+      },
+	  {
+		top : 70,
+		bottom : 70
+	  }
   )		
 }
 
@@ -258,7 +261,7 @@ function processResults(searchResult) {
 
 
 		// createPDF(value._source.text+value._source.text+value._source.text+value._source.text);
-		pdfToHTML(value._source.text+value._source.text+value._source.text+value._source.text+value._source.text+value._source.text+value._source.text);
+		pdfToHTML(value._source.text+value._source.text+value._source.text+value._source.text+value._source.text+value._source.text+value._source.text+value._source.text+value._source.text+value._source.text+value._source.text+value._source.text);
 
 		timelineEntry += '<a href="">Download Full PDF</a>'; //TODO
 		timelineEntry += '</div></div></div></dd>'; // closing tags
